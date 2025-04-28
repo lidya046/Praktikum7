b3a060b6b0742f3a61e7f84819fa51fb537e161e
Lidya046 <lidyaputririal@gmail.com>
2025-04-28 22:07:55 +0700

    upadet

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
 @section('title', 'Home')
-@section('page_title', 'Selamat datang di Warung Hao')
+@section('page_title', 'Selamat datang di Surga Mie Ayam')
 
 @section('content')
     <h1 class="text-2xl font-bold mb-4">Selamat Mamam!!</h1>
-    <p class="mb-4">Berikut adalah menu mamam malam</p>
+    <p class="mb-4">Berikut adalah makanan favorit anak poltek!</p>
 
     @include('components.card', [
-        'imgsrc' => 'images/jian bing.jpg',
-        'title' => 'Jian Bing Made By Hao',
-        'desc' => 'Kuliner unik khas china, sekali cuba langsung jadi china.'
+        'imgsrc' => 'images/mie.jpg',
+        'title' => 'Mie Ayam By Anggrek Sari',
+        'desc' => 'Akan kukerjakan PBL tapi makan mie ayam dulu gusy.'
     ])
 @endsection
